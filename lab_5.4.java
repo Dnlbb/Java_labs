e23406489929b859e6d57fddf0cfc1c7e60f971a
Daniil <147325103+Dnlbb@users.noreply.github.com>
2024-05-06 17:23:19 +0300

    Create lab_5.4.java

---
+++ lab_5.4.java
@@ -0,0 +1,11 @@
+class Bus {
+    int capacity,  routeNumber;
+    double latitude, longitude;
+
+    Bus(int capacity, int routeNumber, double latitude, double longitude) {
+        this.capacity = capacity;
+        this.routeNumber = routeNumber;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
